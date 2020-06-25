32514868113231f8aeab1b12a944bdd66f9b9c93
Josh Wheeler <Josh.Wheeler@arup.com>
2020-06-25 12:34:40 +0100

    Added a StrutObj class which provides the link for get, set, add, removes with the Frew json data. Added a set method.
    Resolves: #6

--- frewpy/models/strut.py
+++ frewpy/models/strut.py
@@ -5,9 +5,42 @@ from typing import Dict, List, Union
 from .exceptions import FrewError
 
 
+class StrutObj():
+    def __init__(
+        self,
+        id: int,
+        flags: int = 0,
+        stage_in: int = -1,
+        stage_out: int = -1,
+        node_strut: int = 1,
+        prestress: float = 0,
+        stiffness: float = 1000000000,
+        angle: float = 0,
+        lever_arm: float = 0,
+        level_strut: float = 0,
+        horz_force: float = 0,
+        is_iba_generated: bool = False,
+        is_seismic: bool = False
+    ):
+        self.id = id
+        self.flags = flags
+        self.stage_in = stage_in
+        self.stage_out = stage_out
+        self.node_strut = node_strut
+        self.prestress = prestress
+        self.stiffness = stiffness
+        self.angle = angle
+        self.lever_arm = lever_arm
+        self.level_strut = level_strut
+        self.horz_force = horz_force
+        self.is_iba_generated = is_iba_generated
+        self.is_seismic = is_seismic
+
+
 class Strut:
-    def __init__(self, json_data):
+    def __init__(self, json_data: dict):
         self.json_data = json_data
+        self.strut_dicts = self.json_data["Struts"]
 
     def _check_json_data(self):
         if not self.json_data.get("Struts", False):
@@ -28,7 +61,12 @@ class Strut:
 
         """
         self._check_json_data()
-        return self.json_data["Struts"]
+        struts = []
+        for i, strut_dict in enumerate(self.strut_dicts, 1):
+            args = list(strut_dict.values())
+            args.insert(0, i)
+            struts.append(StrutObj(*args))
+        return struts
 
     def get_strut_by_node(
         self, node: int
@@ -88,3 +126,25 @@ class Strut:
             raise FrewError(
                 f"There are no struts defined at node {node} in the model"
             )
+
+    def _set_strut_vals(self, strut: StrutObj, strut_dict: dict):
+        # Get list of strut object values excluding the ID
+        new_vals = list(strut.__dict__.values())[1:]
+
+        for key, val in zip(strut_dict.keys(), new_vals):
+            strut_dict[key] = val
+
+    def set(self, struts: Union[StrutObj, List]):
+        if type(struts) == list:
+            for strut, strut_dict in zip(struts, self.strut_dicts):
+                self._set_strut_vals(strut, strut_dict)
+
+        if type(struts) == StrutObj:
+            strut_dict = self.strut_dicts[struts.id - 1]
+            self._set_strut_vals(struts, strut_dict)
+
+    def remove(self, struts: Union[StrutObj, List]):
+        pass
+
+    def add(self, struts: Union[StrutObj, List]):
+        pass
