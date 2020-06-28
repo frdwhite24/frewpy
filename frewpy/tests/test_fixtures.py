734094eb9af04297a87f0606dd467278dc41a8c9
Fred White <Fred.White@arup.com>
2020-06-28 11:10:53 +0100

    amended line length in black configuration for CI

--- frewpy/tests/test_fixtures.py
+++ frewpy/tests/test_fixtures.py
@@ -9,15 +9,15 @@ from frewpy import FrewModel
 
 @pytest.fixture
 def json_data():
-    with open(os.path.join(TEST_DATA, 'test_model_1.json')) as file:
+    with open(os.path.join(TEST_DATA, "test_model_1.json")) as file:
         return json.loads(file.read())
 
 
 @pytest.fixture
 def model():
-    return FrewModel(os.path.join(TEST_DATA, 'test_model_1.json'))
+    return FrewModel(os.path.join(TEST_DATA, "test_model_1.json"))
 
 
 @pytest.fixture
 def empty_model():
-    return FrewModel(os.path.join(TEST_DATA, 'empty_model.json'))
+    return FrewModel(os.path.join(TEST_DATA, "empty_model.json"))
