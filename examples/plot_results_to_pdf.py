ec8546196b172a1eea1e9e72cd7a0a64da9aa131
Fred White <Fred.White@arup.com>
2020-09-23 09:26:26 +0100

    removing unnecessary files and updating gitignore

--- examples/plot_results_to_pdf.py
+++ examples/plot_results_to_pdf.py
@@ -4,10 +4,10 @@ from frewpy import FrewModel
 
 
 # Model file path and the folder in which to save the results
-file_path = r"C:\Users\fred.white\Desktop\example_model.json"
+file_path = r"C:\Users\fred.white\Desktop\SLS B4 South Basement.json"
 folder_path = os.path.dirname(file_path)
 
 # Instantiate the model with FrewModel and then plot the results
 model = FrewModel(file_path)
 model.analyse()
-model.wall.plot_wall_results_pdf(folder_path)
+model.wall.plot_results_pdf(folder_path)
